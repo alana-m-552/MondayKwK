6a1a42bfed2f82c37d7335abbead84a3a39e738b
Apple <apple@Apples-MacBook-Pro-110.local>
2019-07-22 12:02:17 -0700

    initialized git

---
+++ Contents.swift
@@ -0,0 +1,26 @@
+/*oh wow this is so cool.
+ 
+*/
+
+
+
+var favoriteFood = "Dark chocolate"
+if (favoriteFood == "chipotle") {
+    print ("you should try starbucks")
+}
+else if  (favoriteFood == "starbucks") {
+    print ("you should try chipotle")
+}
+else if (favoriteFood == "hamburgers") {
+    print ("you should try smashburger")
+}else {
+    print ("good choice")
+}
+
+
+
+
+
+
+
+
